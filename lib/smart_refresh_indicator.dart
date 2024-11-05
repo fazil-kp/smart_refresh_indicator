0dfa05e0177e97afef62aa888fa78a40e9a7ab04
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-11-05 19:40:46 +0530

    first commit

---
+++ lib/smart_refresh_indicator.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SmartRefreshIndicator extends StatelessWidget {
+  final Widget child;
+  final Future<void> Function() onRefresh;
+  const SmartRefreshIndicator({super.key, required this.child, required this.onRefresh});
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(backgroundColor: Colors.white, color: Colors.red, onRefresh: onRefresh, child: child);
+  }
+}
